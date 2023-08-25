ed50d195b8c76ab14e0c2edafed09c8afecd66cd
Rafael Silva <nightmarezlolzin@gmail.com>
2023-08-25 14:17:27 -0300

    links on midias logo

--- src/components/midias/midias.js
+++ src/components/midias/midias.js
@@ -3,7 +3,7 @@ import "../midias/midiasStyle.css";
 const Midia = () => {
   return (
     <div className="midias">
-      <a href="" target="_blank">
+      <a href="https://www.instagram.com/zg.software/" target="_blank">
         <ion-icon class="ig" name="logo-instagram"></ion-icon>
       </a>
 
