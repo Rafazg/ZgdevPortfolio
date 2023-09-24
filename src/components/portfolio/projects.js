8ff3db278ea830a190855679dd707af8d160e0b4
Rafael Silva <nightmarezlolzin@gmail.com>
2023-09-24 13:39:39 -0300

    adding a new project to the portfolio

--- src/components/portfolio/projects.js
+++ src/components/portfolio/projects.js
@@ -1,5 +1,6 @@
 import "../portfolio/projects.css";
 import project01 from "../../images/Projeto.gif";
+import project02 from "../../images/Projeto02.gif";
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import Grid from "@mui/material/Unstable_Grid2";
@@ -21,6 +22,7 @@ function Projects() {
         gap={10}
       >
         <Grid xs={5}>
+          <h3>Musicalizando</h3>
           <div className="item-1">
             <img className="project-image" src={project01} />
             <ButtonGroup
@@ -36,7 +38,20 @@ function Projects() {
           </div>
         </Grid>
         <Grid xs={5}>
-          <div className="item-2"></div>
+        <h3>Node-API</h3>
+          <div className="item-2">
+            
+            <img className="project-image" src={project02} />
+            <ButtonGroup
+              className="ButtonGroup"
+              disableElevation
+              variant="contained"
+              aria-label="Disabled elevation buttons"
+             
+            >
+              <Button href="https://github.com/Rafazg/node-api" target="_blank">Code</Button>
+            </ButtonGroup>
+          </div>
         </Grid>
       </Grid>
     </section>
